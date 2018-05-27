d55de6bd3ea3f28d21c566cee30ddfa515f7f3c5
AssemblerManiac <amaniac-github@shredco.com>
2018-05-27 15:37:07 -0400

    version update, removal of apiver specific IF statement since long past that version

--- AIResearchGrid/AIResearchGrid.lua
+++ AIResearchGrid/AIResearchGrid.lua
@@ -154,7 +154,7 @@ local styles = {
 	[65] = {stype = AIRG_STYLE_CHAPTERIZED, achiev = 2044}, --Apostle
 	[66] = {stype = AIRG_STYLE_CHAPTERIZED, achiev = 2045}, --Ebonshadow
 	[71] = {stype = AIRG_STYLE_CHAPTERIZED, achiev = 2186}, -- Psijic
-	[72] = {stype = AIRG_STYLE_CHAPTERIZED, achiev = 2187}, -- Sepiarch
+	[72] = {stype = AIRG_STYLE_CHAPTERIZED, achiev = 2187}, -- Sapiarch
 	[75] = {stype = AIRG_STYLE_CHAPTERIZED, achiev = 2285}, -- Pyandonean
 }
 
@@ -824,7 +824,7 @@ local function InitUI()
 	AIRG_UI.btnBlacksmithing:SetNormalTexture("/esoui/art/icons/ability_smith_007.dds")
 	AIRG_UI.btnBlacksmithing:SetMouseOverTexture("esoui/art/buttons/generic_highlight.dds")
 	AIRG_UI.btnBlacksmithing:SetHandler("OnClicked", function(self) OnCraftSelected(CRAFTING_TYPE_BLACKSMITHING) end)
-	AIRG_UI.btnBlacksmithing:SetHandler("OnMouseEnter", function(self) ZO_Tooltips_ShowTextTooltip(self, TOP, GetString(SI_ITEMFILTERTYPE13)) end)
+	AIRG_UI.btnBlacksmithing:SetHandler("OnMouseEnter", function(self) ZO_Tooltips_ShowTextTooltip(self, RIGHT, GetString(SI_ITEMFILTERTYPE13)) end)
 	AIRG_UI.btnBlacksmithing:SetHandler("OnMouseExit", function (self) ZO_Tooltips_HideTextTooltip() end)
 
 	-- CREATE BUTTON FOR PROFESSION: WOODWORKING
@@ -839,7 +839,7 @@ local function InitUI()
 	AIRG_UI.btnWoodworking:SetNormalTexture("/esoui/art/icons/ability_tradecraft_009.dds")
 	AIRG_UI.btnWoodworking:SetMouseOverTexture("esoui/art/buttons/generic_highlight.dds")
 	AIRG_UI.btnWoodworking:SetHandler("OnClicked", function(self) OnCraftSelected(CRAFTING_TYPE_WOODWORKING) end)
-	AIRG_UI.btnWoodworking:SetHandler("OnMouseEnter", function(self) ZO_Tooltips_ShowTextTooltip(self, TOP, GetString(SI_ITEMFILTERTYPE14)) end)
+	AIRG_UI.btnWoodworking:SetHandler("OnMouseEnter", function(self) ZO_Tooltips_ShowTextTooltip(self, RIGHT, GetString(SI_ITEMFILTERTYPE14)) end)
 	AIRG_UI.btnWoodworking:SetHandler("OnMouseExit", function (self) ZO_Tooltips_HideTextTooltip() end)
 
 	-- CREATE BUTTON FOR PROFESSION: CLOTHING
@@ -854,7 +854,7 @@ local function InitUI()
 	AIRG_UI.btnClothing:SetNormalTexture("/esoui/art/icons/ability_tradecraft_008.dds")
 	AIRG_UI.btnClothing:SetMouseOverTexture("esoui/art/buttons/generic_highlight.dds")
 	AIRG_UI.btnClothing:SetHandler("OnClicked", function(self) OnCraftSelected(CRAFTING_TYPE_CLOTHIER) end)
-	AIRG_UI.btnClothing:SetHandler("OnMouseEnter", function(self) ZO_Tooltips_ShowTextTooltip(self, TOP, GetString(SI_ITEMFILTERTYPE15)) end)
+	AIRG_UI.btnClothing:SetHandler("OnMouseEnter", function(self) ZO_Tooltips_ShowTextTooltip(self, RIGHT, GetString(SI_ITEMFILTERTYPE15)) end)
 	AIRG_UI.btnClothing:SetHandler("OnMouseExit", function (self) ZO_Tooltips_HideTextTooltip() end)
 
 	-- CREATE BUTTON FOR PROFESSION: JEWELRY CRAFTING
@@ -869,7 +869,7 @@ local function InitUI()
 	AIRG_UI.btnJewelryCrafting:SetNormalTexture("/esoui/art/icons/passive_jewelerengraver.dds")
 	AIRG_UI.btnJewelryCrafting:SetMouseOverTexture("esoui/art/buttons/generic_highlight.dds")
 	AIRG_UI.btnJewelryCrafting:SetHandler("OnClicked", function(self) OnCraftSelected(CRAFTING_TYPE_JEWELRYCRAFTING) end)
-	AIRG_UI.btnJewelryCrafting:SetHandler("OnMouseEnter", function(self) ZO_Tooltips_ShowTextTooltip(self, TOP, GetString(SI_ITEMFILTERTYPE24)) end)
+	AIRG_UI.btnJewelryCrafting:SetHandler("OnMouseEnter", function(self) ZO_Tooltips_ShowTextTooltip(self, RIGHT, GetString(SI_ITEMFILTERTYPE24)) end)
 	AIRG_UI.btnJewelryCrafting:SetHandler("OnMouseExit", function (self) ZO_Tooltips_HideTextTooltip() end)
 
 	-- BUILD THE TRAIT LABELS ON THE LEFT-SIDE using built-in language strings
@@ -1148,9 +1148,7 @@ local function OnAddonLoaded(_, addonName)
 		EVENT_MANAGER:RegisterForEvent(ADDON_NAME, EVENT_SMITHING_TRAIT_RESEARCH_STARTED, BuildMatrix)
 		EVENT_MANAGER:RegisterForEvent(ADDON_NAME, EVENT_SMITHING_TRAIT_RESEARCH_COMPLETED, BuildMatrix)
 
-		if apiVersion >= 100021 then
-			EVENT_MANAGER:RegisterForEvent(ADDON_NAME, EVENT_SKILLS_FULL_UPDATE, BuildMatrix)
-		end
+		EVENT_MANAGER:RegisterForEvent(ADDON_NAME, EVENT_SKILLS_FULL_UPDATE, BuildMatrix)
 
 		EVENT_MANAGER:RegisterForEvent(ADDON_NAME, EVENT_PLAYER_ACTIVATED, BuildMatrix)
 		EVENT_MANAGER:RegisterForEvent(ADDON_NAME, EVENT_STYLE_LEARNED, BuildMatrix)
